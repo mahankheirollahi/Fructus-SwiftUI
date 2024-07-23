b13dfdc3ff75c6edcb1f1091189ebe3afc46a3de
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-24 02:24:03 +0300

    Add onboarding view inside the main view

---
+++ Fructus-SwiftUI/App/Fructus_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus_SwiftUIApp.swift
+//  Fructus-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Fructus_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
